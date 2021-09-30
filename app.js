3140be7a53b8e6cafa6eacc2b1448c5920f566b9
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-09-29 17:38:29 -0700

    Forgot to ACP, makes the whole lab

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // import functions and grab DOM elements
+const nameInput = document.getElementById('name');
+const button = document.getElementById('generate');
+const firstName = document.getElementById('first-name');
+
+const blueButton = document.getElementById('blue-button');
+const greenButton = document.getElementById('green-button');
+const orangeButton = document.getElementById('orange-button');
+const redButton = document.getElementById('red-button');
+const americaButton = document.getElementById('america-button');
+
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
 
 // initialize global state
 
@@ -6,3 +18,29 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+button.addEventListener('click', ()=> {
+    let newName = nameInput.value; 
+    firstName.textContent = newName; 
+});
+
+blueButton.addEventListener('click', ()=> {
+    header.style.backgroundColor = 'blue';
+    footer.style.backgroundColor = 'blue';
+});
+greenButton.addEventListener('click', ()=> {
+    header.style.backgroundColor = 'green';
+    footer.style.backgroundColor = 'green';
+});
+orangeButton.addEventListener('click', ()=> {
+    header.style.backgroundColor = 'orange';
+    footer.style.backgroundColor = 'orange';
+});
+redButton.addEventListener('click', ()=> {
+    header.style.backgroundColor = 'red';
+    footer.style.backgroundColor = 'red';
+});
+americaButton.addEventListener('click', ()=> {
+    header.style.backgroundColor = 'red';
+    footer.style.backgroundColor = 'blue';
+});
